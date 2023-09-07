08444ca6659c3ec9b6b670cf2ab000a9ba9d67a4
Owner <joywin16@gmail.com>
2023-09-07 21:47:58 +0100

    day 57 index page built with calling data, menu_item_detail.html page built

--- restaurent_menu/urls.py
+++ restaurent_menu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.MenuList.as_view(), name='home')      # .as_view since MenuList is a class and class doesn't return
+    path('', views.MenuList.as_view(), name='home'),   # .as_view since MenuList is a class and class doesn't return
+    path('item/<int:pk>', views.MenuItemDetail.as_view(), name='menu_item')
 ]
